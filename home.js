521b3597fa33a73765932e4eedd8d223c5483ba0
Golois Mouelet <gomouele@microsoft.com>
2018-12-10 19:10:53 -0500

    remove slim jsquery

--- home.js
+++ home.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
+
     $("#submitBtn").click(function(){
+
       var ml_prediction_url = 'http://40.85.149.250:80/score';
       clearConfirmationLabel();
+
       var requestData = {'request': $('#comments').val()};
+
       $.post(ml_prediction_url, requestData, function(data){
         var msg = '';
         if(data && data.department && data.probability) {
